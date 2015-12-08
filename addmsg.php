abedfba9e823d30faed9c775cf8a8b0a378dac20
brian <xu65k6djo6@gmail.com>
2015-12-09 01:21:04 +0800

    remove array

--- addmsg.php
+++ addmsg.php
@@ -8,9 +8,7 @@
     <?php 
         $fp = fopen('msgbf.csv', 'a+');
 
-        $data = array($_POST['name'],$_POST['email'],$_POST['title'],$_POST['comments'] );//put the value we get from the text into data array
-
-        if ($data[0] === '' && $data[1] === '' && $data[2] === '' && $data[3] === ''){//if nothing input
+        if ($_POST['name'] === '' && $_POST['email'] === '' && $_POST['title'] === '' && $_POST['comments'] === ''){//if nothing input
 
             echo "please input something!!";
 
@@ -21,15 +19,18 @@
             date_default_timezone_set('Asia/Taipei');   //get time
             $date_time = date("Y-m-d H:i:s");           
 
-            $data[4] = $date_time;
-
             echo "add message successful"; 
 
             ?>
             <a href="index.php">確認</a>
             <?php
 
-            fputcsv($fp, $data);    //save data into msgbf.csv          
+            fputcsv($fp, [$_POST['name'],
+                         $_POST['email'],
+                         $_POST['title'],
+                         $_POST['comments'],
+                         $date_time]
+                    );    //save data into msgbf.csv          
         }
 
         fclose($fp);
